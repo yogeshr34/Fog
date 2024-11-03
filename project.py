d18fe2a26b43342ee124c3766f77e050eba05299
yogeshr34 <yogesh2605007@gmail.com>
2024-11-03 20:21:53 +0530

    03/11/2024 20:21:54

--- project.py
+++ project.py
@@ -13,7 +13,6 @@ ADC_MAX_VALUE = 4095  # 12-bit ADC
 # Current Sensor Configuration (ACS712)
 current_adc_pin = ADC(Pin(33))  # Adjust GPIO pin as needed
 current_adc_pin.atten(ADC.ATTN_11DB)
-
 SENSITIVITY = 185  # Sensitivity in mV per A for ACS712-5A (use 100 for 20A, 66 for 30A)
 V_ZERO = V_REF / 2  # Adjusted later during calibration if necessary
 
