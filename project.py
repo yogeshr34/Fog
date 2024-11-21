a3204acb1f966f170578269906ddb0723a3b27fa
yogeshr34 <yogesh2605007@gmail.com>
2024-11-21 14:39:05 +0530

    21/11/2024 14:39:03

--- project.py
+++ project.py
@@ -26,8 +26,6 @@ def calibrate_zero():
         time.sleep(0.01)
     zero_adc_value = total / samples
     return (zero_adc_value / ADC_MAX_VALUE) * V_REF
-
-# Set the midpoint voltage based on calibration
 V_ZERO = calibrate_zero()
 
 def read_voltage():
